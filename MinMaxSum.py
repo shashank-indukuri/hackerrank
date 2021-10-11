bd5a42f0a016ff31e089467802a15c481477bc4c
Shashank Indukuri <65766473+shashank-indukuri@users.noreply.github.com>
2021-10-11 15:51:45 -0500

    Min-Max Sum

---
+++ MinMaxSum.py
@@ -0,0 +1,32 @@
+# Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+# Example
+
+# The minimum sum is  and the maximum sum is . The function prints
+
+# 16 24
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    arr.sort()
+    print(sum(arr[:4]), sum(arr[1:]))
+            
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
