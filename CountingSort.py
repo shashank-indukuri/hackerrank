553f55ea11d26416aa34658e32efecaf8cc88b92
Shashank Indukuri <65766473+shashank-indukuri@users.noreply.github.com>
2021-10-13 23:06:54 -0500

    Counting Sort 1

---
+++ CountingSort.py
@@ -0,0 +1,51 @@
+# Comparison Sorting
+# Quicksort usually has a running time of , but is there an algorithm that can sort even faster? In general, this is not possible. Most sorting algorithms are comparison sorts, i.e. they sort a list just by comparing the elements to one another. A comparison sort algorithm cannot beat  (worst-case) running time, since  represents the minimum number of comparisons needed to know where to place each element. For more details, you can see these notes (PDF).
+
+# Alternative Sorting
+# Another sorting method, the counting sort, does not require comparison. Instead, you create an integer array whose index range covers the entire range of values in your array to sort. Each time a value occurs in the original array, you increment the counter at that index. At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
+
+# Example
+
+# All of the values are in the range , so create an array of zeros, . The results of each iteration follow:
+
+# i	arr[i]	result
+# 0	1	[0, 1, 0, 0]
+# 1	1	[0, 2, 0, 0]
+# 2	3	[0, 2, 0, 1]
+# 3	2	[0, 2, 1, 1]
+# 4	1	[0, 3, 1, 1]
+# The frequency array is . These values can be used to create the sorted array as well: .
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    r = [0]*100
+    for i in arr:
+        r[i] += 1
+    return r
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
